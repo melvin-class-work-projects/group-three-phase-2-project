a2650b6eefe816b7e38bb7be79835e89756d404b
iamchacha021 <iamchacha021@gmail.com>
2023-01-24 00:31:45 +0300

    delete Navbar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import '../stylesheet/App.css';
 import Home from './Home';
-import NavBar from './NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      
       <br /><br /><br />
       <Home />
     </div>
