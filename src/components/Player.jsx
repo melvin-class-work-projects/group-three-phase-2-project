57fabe8133a004e3d266b8f5a6c701f6265b6634
Marcus <marcus.kariuki@student.moringaschool.com>
2023-01-25 17:39:03 +0300

    edit personal player info page edit db.json "strong foot" to "foot"

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -28,22 +28,27 @@ const Player = () => {
           <>
             <div className="col-md-6">
               <img
-                src={player.image}
+                src={player.profile}
                 alt={player.name}
-                height="400px"
-                width="400px"
               />
             </div>
             <div className="col-md-6">
               <h4 className="text-uppercase text-black-50">{player.type}</h4>
               <h1 className="display-5">{player.name}</h1>
               <p className="lead fw-bolder">
-                Club: {player.club} <br/><br/> Salary: {player.salary}
+                <ul>
+                <li>Club: {player.club}</li>
+                <li>League: {player.league}</li> 
+                <li>Salary: {player.salary}</li>
+                <li>Height: {player.height}</li>
+                <li>Strong foot: {player.foot}</li>
+                </ul>
               </p>
               <h3 className="display-6 my-4 fw-bold">
                 ${player.value}
               </h3>
               <p className="lead">{player.description}</p>
+              
               <button className="btn btn-dark">
                 Bid
               </button>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './stylesheet/index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+// import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 // import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
